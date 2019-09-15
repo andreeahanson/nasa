afa2482431585efc8be57216c100cf9d7fd90921
Andreea Hanson <andreea.hanson7@gmail.com>
2019-09-15 12:18:42 -0600

    Display previous days of the month on page

--- apiCalls.js
+++ apiCalls.js
@@ -13,3 +13,17 @@ export const fetchPictureOfTheDay = async () => {
     throw new Error(error.message);
   }
 };
+
+export const fetchPictureOfTheMonth = async date => {
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Error uploading the Astronomy Picture of the Day");
+    }
+    const pictureData = await response.json();
+    return pictureData;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
---
+++ src/views/PreviousDatesPictures.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div v-for="date in dates">
+      <h3>{{date}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PreviousDatesPictures",
+  props: ["dates"]
+}
+</script>
+
